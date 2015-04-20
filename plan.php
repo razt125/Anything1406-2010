e8b1407fdcca8b9cd8cb96991b827db24c1d90a2
Ryan Swincer <ryan.swincer@my.jcu.edu.au>
2015-04-20 16:14:01 +1000

    Update plan.php

--- plan.php
+++ plan.php
@@ -1,25 +1,44 @@
 <!doctype html>
 <html>
 <head>
-<style>
 
+<style type="text/css">
+body {
+	text-align: center;	
+	background-color: #3B2021;
+}
+div {
+	background-color: #FFDCDD;
+	margin-top: 50px;
+	margin-right: 50px;
+	margin-bottom: 50px;
+	margin-left: 50px;
+	padding-top: 20px;
+	padding-right: 20px;
+	padding-bottom: 20px;
+	padding-left: 20px;
+	border-radius: 50px;
+	border: medium groove #535353;
+}
 </style>
 <title>A1 Project Plan for Student Name</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"></head>
 
 <body>
+<div>
   <h1>CP1406/CP2010 - 2015 - Major Project - Project Plan</h1>
   <h1> Townsville Community Music Centre</h1>
-  <p><strong>Name:</strong> Anything - Group 11<br>
-  <strong>Login:</strong> jc321030, jc320668, </p>
+<p><strong>Name:</strong> Anything - Group 11<br>
+  <strong>Login:</strong> jc321030, jc320668, jc321013, jc260754, jc239342</p>
 <h2>Mission Statement</h2>
-  <p>...</p>
+<p>To increase number of members and musicians using the site in order to raise awareness for community events, gaining volunteers and increasing concert revenues, also to increase site donations and lower upkeep and mainteneance of the site.</p>
   <h2>Success Evaluation</h2>
-<p> ...</p>
+<p>monitor increase in: donations, volunteers, members, concert attendees </p>
 <h2>Target Audience</h2>
-<p>...</p>
+<p>Mature music enthusiasts </p>
 <h2>Site Flowchart (Structure Diagram)</h2>
 <p> ...   <br>
 </p>
+</div>
 </body>
 </html>
