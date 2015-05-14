44c776518f88479bf9ddc6ab6196712f028b69b1
Luke Giliberto <luke.giliberto@my.jcu.edu.au>
2015-05-14 21:27:42 +1000

    created events page

---
+++ events.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>index.html</title>
+<script scr="scripts.js"></script>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body onload="rotate()">
+<h1>Townsville Community Music Centre</h1>
+<nav class="navbar">
+<p>Home</p>
+<p>Events </p>
+<p>Bulletin Board</p>
+<p>Musicians</p>
+<p> About Us</p>
+</nav>
+
+<footer>
+<p>Contact Info</p>
+<p>&nbsp;</p>
+</footer>
+</body>
+</html>
