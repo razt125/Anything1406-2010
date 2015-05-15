3390e35e5f0b39357e1ccb8743a6fac73429e28d
Luke Giliberto <luke.giliberto@my.jcu.edu.au>
2015-05-15 16:45:37 +1000

    created template for future pages

---
+++ template.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>index.html</title>
+<script scr="scripts.js"></script>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body onload="rotate()">
+
+<h1>Townsville Community Music Centre</h1>
+
+<nav class="navbar">
+  <p id="activepage">Home</p>
+  <p>Events </p>
+  <p>Bulletin Board</p>
+  <p>Musicians</p>
+  <p> About Us</p>
+</nav>
+<br><br>
+
+<div id="pagecontent">
+</div>
+
+<footer>
+	<div id= contacts> 
+  		<h3>Contact Info:</h3>
+  		 <p>Phone:   07 4724 2086<br>
+         Mobile:    0402 255 182<br><br>
+         
+ 		 Postal Address  PO Box:<br>
+         1006, Townsville, Qld 4810<br>
+         Address Townsville Civic Theatre:<br> 
+         41 Boundary Street, Townsville, Qld 4810<br>
+         Office hours: 9:30 - 2:30 Monday - Wednesday</p>
+	</div>
+    <div id= sponsors>
+    <h3>Sponsors:</h3>
+    <h4>Townsville City Council:</h4>
+     <p> http://www.townsville.qld.gov.au/Pages/default.aspx</p>
+      <h4>Queensland Government Gambling Community Benefit Fund:</h4>
+      <p>http://www.justice.qld.gov.au/corporate/sponsorships-and-grants/grants/community-benefit-funding-programs</p>
+    </div>
+</footer>
+</body>
+</html>
