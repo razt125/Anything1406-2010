7aa59d451d45ff02d2d7893605ae4fd5463364d8
Luke Giliberto <luke.giliberto@my.jcu.edu.au>
2015-05-15 16:40:03 +1000

    contacts/sponsors content, rotating banner location

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <h1>Townsville Community Music Centre</h1>
 
 <nav class="navbar">
-  <p>Home</p>
+  <p id="activepage">Home</p>
   <p>Events </p>
   <p>Bulletin Board</p>
   <p>Musicians</p>
@@ -55,9 +55,25 @@
 </div>
 </div>
 
-<footer> 
-  <p>Contact Info</p>
-  <p>&nbsp;</p>
+<footer>
+	<div id= contacts> 
+  		<h3>Contact Info:</h3>
+  		 <p>Phone:   07 4724 2086<br>
+         Mobile:    0402 255 182<br><br>
+         
+ 		 Postal Address  PO Box:<br>
+         1006, Townsville, Qld 4810<br>
+         Address Townsville Civic Theatre:<br> 
+         41 Boundary Street, Townsville, Qld 4810<br>
+         Office hours: 9:30 - 2:30 Monday - Wednesday</p>
+	</div>
+    <div id= sponsors>
+    <h3>Sponsors:</h3>
+    <h4>Townsville City Council:</h4>
+     <p> http://www.townsville.qld.gov.au/Pages/default.aspx</p>
+      <h4>Queensland Government Gambling Community Benefit Fund:</h4>
+      <p>http://www.justice.qld.gov.au/corporate/sponsorships-and-grants/grants/community-benefit-funding-programs</p>
+    </div>
 </footer>
 </body>
 </html>
