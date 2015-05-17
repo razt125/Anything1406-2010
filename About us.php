9f24409333fe5f5487961b1299741ebfba1e1259
jc321013 <jared.marcolongo@my.jcu.edu.au>
2015-05-17 21:14:26 +1000

    Create About us.php

---
+++ About us.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>index.html</title>
+<script scr="scripts.js"></script>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body onload="rotate()">
+<h1>Townsville Community Music Centre</h1>
+<nav class="navbar">
+<p>Home</p>
+<p>Events </p>
+<p>Bulletin Board</p>
+<p>Musicians</p>
+<p> About Us</p>
+</nav>
+<div id="pagecontent">
+<div id="Looking Back- The first 30 years!>  </div>
+<div id="</div>
+<div id="</div>
+</div>
+<footer>
+<p>Contact Info</p>
+<p>&nbsp;</p>
+</footer>
+</body>
+</html>
