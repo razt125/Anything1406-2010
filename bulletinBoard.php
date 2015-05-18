0672842604645519eb47d560822fb10834551b42
Luke Giliberto <luke.giliberto@my.jcu.edu.au>
2015-05-18 16:52:49 +1000

    Update bulletinBoard.php

--- bulletinBoard.php
+++ bulletinBoard.php
@@ -1,77 +1,78 @@
-
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
 <title>index.html</title>
-<script scr="scripts.js"></script>
+<script scr="script.js"></script>
 <link href="styles.css" rel="stylesheet" type="text/css">
-<link href="Untitled-1.php.css" rel="stylesheet" type="text/css">
-@charset "utf-8";
-#content {
-	text-align: center;
-	margin-left: 20px;
-	margin-right: 20px;
-	color: #167CD0;
-	margin-top: 30px;
-	margin-bottom: 30px;
-}
-
-}
-
-#Message {
-	margin-top: 30px;
-	margin-bottom: 30px;
-	margin-right: 20px;
-	margin-left: 20px;
-	padding-top: 10px;
-	padding-bottom: 10px;
-	color: #2075CF;
-	text-shadow: 0px 0px #08090A;
-	border-color: #7B7E87;
-}
-#Message 2 {
-	margin-top: 30px;
-	margin-bottom: 30px;
-	margin-right: 20px;
-	margin-left: 20px;
-	padding-top: 10px;
-	padding-bottom: 10px;
-	color: #2075CF;
-	text-shadow: 0px 0px #08090A;
-	border-color: #7B7E87;
-}
-#Message 3 {
-	margin-top: 30px;
-	margin-bottom: 30px;
-	margin-right: 20px;
-	margin-left: 20px;
-	padding-top: 10px;
-	padding-bottom: 10px;
-	color: #2075CF;
-	text-shadow: 0px 0px #08090A;
-	border-color: #7B7E87;
-
 </head>
 <body onload="rotate()">
 <h1>Townsville Community Music Centre</h1>
 <nav class="navbar">
-<p>Home</p>
-<p>Events </p>
-<p><a href="Bulliten board.php">Bulletin Board</a></p>
-<p>Musicians</p>
-<p> About Us</p>
+<a href="index.php"><p id="activepage">Home</p></a>
+<a href="events.php"><p>Events</p></a>
+<a href="bulletin.php"><p>Bulletin Board</p></a>
+<a href="musicians.php"><p>Musicians</p></a>
+<a href="aboutUs.php"><p>About Us</p></a>
 </nav>
+<br><br>
 <div id="pagecontent">
-<div id="content"> Townsville Community Music Centre's bulletin board is a message source provided to stay in touch with the organisations newest events and announcements. It is a way to advertise usic events and promotions such as Music teaching, sales of music instruments and other messages.
-<div id="Message"> Message from... </div>
-<div id="Message 2"> Message </div>
-<div id="Message 3">Link to message or announcement </div>
-
+<br>
+<div id="section1">
+<p>Help us out by becoming a member, donating or volunteering </p>
+</div>
+<div id= section2>
+<p id="Button1">Sign Up</p>
+<p id="Button2">Donate</p>
+<p id="Button3">Volunteer</p>
+</div>
+<div id= section3>
+<h2>Announcements</h2>
+<p>Announcement 1: (text)</p>
+<p>Announcement 2: (text)</p>
+<p>Announcement 3: (text)</p>
+<p>Announcement 4: (text)</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+<div id= section4>
+<center>
+<img name="adBanner" src="rotating banner/image1.png"/>
+</center>
+</div>
 </div>
 <footer>
-<p>Contact Info</p>
-<p>&nbsp;</p>
+<div id= contacts>
+<h3>Contact Info:</h3>
+<p>Phone: 07 4724 2086<br>
+Mobile: 0402 255 182<br><br>
+Postal Address PO Box:<br>
+1006, Townsville, Qld 4810<br>
+Address Townsville Civic Theatre:<br>
+41 Boundary Street, Townsville, Qld 4810<br>
+Office hours: 9:30 - 2:30 Monday - Wednesday</p>
+</div>
+<div id= sponsors>
+<h3>Sponsors:</h3>
+<h4>Townsville City Council:</h4>
+<p> http://www.townsville.qld.gov.au/Pages/default.aspx</p>
+<h4>Queensland Government Gambling Community Benefit Fund:</h4>
+<p>http://www.justice.qld.gov.au/corporate/sponsorships-and-grants/grants/community-benefit-funding-programs</p>
+</div>
+<div id="sitemap">
+<p>link 1</p>
+<p>link 2</p>
+<p>link 3</p>
+<p>link 4</p>
+<p>link 5</p>
+<p>link 6</p>
+<p>link 7</p>
+</div>
 </footer>
 </body>
 </html>
