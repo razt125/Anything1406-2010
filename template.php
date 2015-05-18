73a47070b088583a79c0c4a0ce92a524cd9663e3
Luke Giliberto <luke.giliberto@my.jcu.edu.au>
2015-05-18 16:55:41 +1000

    Update template.php

--- template.php
+++ template.php
@@ -3,10 +3,9 @@
 <head>
 <meta charset="utf-8">
 <title>index.html</title>
-<script scr="script.js"></script>
 <link href="styles.css" rel="stylesheet" type="text/css">
 </head>
-<body onload="rotate()">
+<body>
 <h1>Townsville Community Music Centre</h1>
 <nav class="navbar">
 <a href="index.php"><p id="activepage">Home</p></a>
@@ -16,36 +15,10 @@
 <a href="aboutUs.php"><p>About Us</p></a>
 </nav>
 <br><br>
+
 <div id="pagecontent">
-<br>
-<div id="section1">
-<p>Help us out by becoming a member, donating or volunteering </p>
 </div>
-<div id= section2>
-<p id="Button1">Sign Up</p>
-<p id="Button2">Donate</p>
-<p id="Button3">Volunteer</p>
-</div>
-<div id= section3>
-<h2>Announcements</h2>
-<p>Announcement 1: (text)</p>
-<p>Announcement 2: (text)</p>
-<p>Announcement 3: (text)</p>
-<p>Announcement 4: (text)</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-</div>
-<div id= section4>
-<center>
-<img name="adBanner" src="rotating banner/image1.png"/>
-</center>
-</div>
-</div>
+
 <footer>
 <div id= contacts>
 <h3>Contact Info:</h3>
