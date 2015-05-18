e0bbdedd8f6c1e5e752e740861b5edeaf640792d
jc321013 <jared.marcolongo@my.jcu.edu.au>
2015-05-18 15:56:22 +1000

    Update bulletinBoard.php

--- bulletinBoard.php
+++ bulletinBoard.php
@@ -6,6 +6,52 @@
 <title>index.html</title>
 <script scr="scripts.js"></script>
 <link href="styles.css" rel="stylesheet" type="text/css">
+<link href="Untitled-1.php.css" rel="stylesheet" type="text/css">
+@charset "utf-8";
+#content {
+	text-align: center;
+	margin-left: 20px;
+	margin-right: 20px;
+	color: #167CD0;
+	margin-top: 30px;
+	margin-bottom: 30px;
+}
+
+}
+
+#Message {
+	margin-top: 30px;
+	margin-bottom: 30px;
+	margin-right: 20px;
+	margin-left: 20px;
+	padding-top: 10px;
+	padding-bottom: 10px;
+	color: #2075CF;
+	text-shadow: 0px 0px #08090A;
+	border-color: #7B7E87;
+}
+#Message 2 {
+	margin-top: 30px;
+	margin-bottom: 30px;
+	margin-right: 20px;
+	margin-left: 20px;
+	padding-top: 10px;
+	padding-bottom: 10px;
+	color: #2075CF;
+	text-shadow: 0px 0px #08090A;
+	border-color: #7B7E87;
+}
+#Message 3 {
+	margin-top: 30px;
+	margin-bottom: 30px;
+	margin-right: 20px;
+	margin-left: 20px;
+	padding-top: 10px;
+	padding-bottom: 10px;
+	color: #2075CF;
+	text-shadow: 0px 0px #08090A;
+	border-color: #7B7E87;
+
 </head>
 <body onload="rotate()">
 <h1>Townsville Community Music Centre</h1>
@@ -19,8 +65,8 @@
 <div id="pagecontent">
 <div id="content"> Townsville Community Music Centre's bulletin board is a message source provided to stay in touch with the organisations newest events and announcements. It is a way to advertise usic events and promotions such as Music teaching, sales of music instruments and other messages.
 <div id="Message"> Message from... </div>
-<div id="Message"> Message </div>
-<div id="message">Link to message or announcement </div>
+<div id="Message 2"> Message </div>
+<div id="Message 3">Link to message or announcement </div>
 
 </div>
 <footer>
