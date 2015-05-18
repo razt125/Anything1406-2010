0786cf6a7a2bfbf2ee0ebd4c019fd2080d75b499
Luke Giliberto <luke.giliberto@my.jcu.edu.au>
2015-05-18 16:58:28 +1000

    Update and rename Musicians.php to musicians.php

---
+++ musicians.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>index.html</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Townsville Community Music Centre</h1>
+<nav class="navbar">
+<a href="index.php"><p id="activepage">Home</p></a>
+<a href="events.php"><p>Events</p></a>
+<a href="bulletin.php"><p>Bulletin Board</p></a>
+<a href="musicians.php"><p>Musicians</p></a>
+<a href="aboutUs.php"><p>About Us</p></a>
+</nav>
+<br><br>
+<div id="pagecontent">
+</div>
+<footer>
+<div id= contacts>
+<h3>Contact Info:</h3>
+<p>Phone: 07 4724 2086<br>
+Mobile: 0402 255 182<br><br>
+Postal Address PO Box:<br>
+1006, Townsville, Qld 4810<br>
+Address Townsville Civic Theatre:<br>
+41 Boundary Street, Townsville, Qld 4810<br>
+Office hours: 9:30 - 2:30 Monday - Wednesday</p>
+</div>
+<div id= sponsors>
+<h3>Sponsors:</h3>
+<h4>Townsville City Council:</h4>
+<p> http://www.townsville.qld.gov.au/Pages/default.aspx</p>
+<h4>Queensland Government Gambling Community Benefit Fund:</h4>
+<p>http://www.justice.qld.gov.au/corporate/sponsorships-and-grants/grants/community-benefit-funding-programs</p>
+</div>
+<div id="sitemap">
+<p>link 1</p>
+<p>link 2</p>
+<p>link 3</p>
+<p>link 4</p>
+<p>link 5</p>
+<p>link 6</p>
+<p>link 7</p>
+</div>
+</footer>
+</body>
+</html>
