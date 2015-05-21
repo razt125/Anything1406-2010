7735fd4944ac6335a775268b8a2016b08196c2bc
Luke Giliberto <luke.giliberto@my.jcu.edu.au>
2015-05-21 20:33:46 +1000

    Update events.php

--- events.php
+++ events.php
@@ -3,26 +3,58 @@
 <head>
 <meta charset="utf-8">
 <title>index.html</title>
-<script scr="scripts.js"></script>
 <link href="styles.css" rel="stylesheet" type="text/css">
 </head>
-<body onload="rotate()">
+<body>
+<header>
+  <div class="logo"><img src="images/logo/logo.jpg" alt="Logo" style="width:150px;height:100px"></div>
 <h1>Townsville Community Music Centre</h1>
+<form>
+  Username:
+  <input type="text" username="username">
+  Password:
+  <input type="text" password="password">
+</form>
+</header>
 <nav class="navbar">
 <a href="index.php"><p>Home</p></a>
 <a href="events.php"><p id="activepage">Events</p></a>
 <a href="bulletin.php"><p>Bulletin Board</p></a>
 <a href="musicians.php"><p>Musicians</p></a>
 <a href="aboutUs.php"><p>About Us</p></a>
-</nav>
+</nav><br><br>
 <div id="pagecontent">
-<div id="mainevent">Link to main event </div>
-<div id="event1">Link to next event</div>
-<div id="event2">Link to next event</div>
+<div id="mainevent"><img src="images/events/event0.jpg" alt="Main Event" style="width:600px;height:300px"></div>
+<div id="event1"><img src="images/events/event1.jpg" alt="event1" style="width:400px;height:200px"></div>
+<div id="event2"><a href="musician1.php"><img src="images/events/event2.jpg" alt="event2" style="width:400px;height:200px"></a></div>
 </div>
 <footer>
-<p>Contact Info</p>
-<p>&nbsp;</p>
+<div id= contacts>
+<h3>Contact Info:</h3>
+<p>Phone: 07 4724 2086<br>
+Mobile: 0402 255 182<br><br>
+Postal Address PO Box:<br>
+1006, Townsville, Qld 4810<br>
+Address Townsville Civic Theatre:<br>
+41 Boundary Street, Townsville, Qld 4810<br>
+Office hours: 9:30 - 2:30 Monday - Wednesday</p>
+</div>
+<div id= sponsors>
+<h3>Sponsors:</h3>
+<h4>Townsville City Council:</h4>
+<p> http://www.townsville.qld.gov.au/Pages/default.aspx</p>
+<h4>Queensland Government Gambling Community Benefit Fund:</h4>
+<p>http://www.justice.qld.gov.au/corporate/sponsorships-and-grants/grants/community-benefit-funding-programs</p>
+</div>
+
+<div id="sitemap">
+<a href="index.php"><p id="activepage">Home</p></a>
+<a href="events.php"><p>Events</p></a>
+<a href="bulletin.php"><p>Bulletin Board</p></a>
+<a href="musicians.php"><p>Musicians</p></a>
+<a href="aboutUs.php"><p>About Us</p></a>
+<p>link 6</p>
+</div>
 </footer>
 </body>
 </html>
