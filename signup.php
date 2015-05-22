6bd4a9e20b130c37a51505a2e5fa63d3955622ed
Luke Giliberto <luke.giliberto@my.jcu.edu.au>
2015-05-22 16:19:51 +1000

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>index.html</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<header>
+  <div class="logo"><img src="images/logo/logo.jpg" alt="Logo" style="width:150px;height:100px"></div>
+<h1>Townsville Community Music Centre</h1>
+<form>
+  Username:
+  <input type="text" username="username">
+  Password:
+  <input type="text" password="password">
+</form>
+</header>
+
+<nav class="navbar">
+<a href="index.php"><p id="activepage">Home</p></a>
+<a href="events.php"><p>Events</p></a>
+<a href="bulletin.php"><p>Bulletin Board</p></a>
+<a href="musicians.php"></a>
+<a href="aboutUs.php">
+<p>Musicians</p>
+<p>About Us</p></a>
+</nav>
+<br><br>
+<div id="pagecontent">
+<div id="signup">
+<h2>Sign Up</h2>
+<form>
+First Name:<br>
+<input type="text" firstname="firstname"><br>
+Last Name:<br>
+<input type="text" lastname="lastname"><br>
+Address:<br>
+<input type="text" address="address"><br>
+Home Phone Number:<br>
+<input type="text" homephone="homephone"><br>
+Mobile Phone Number:
+<br><input type="text" mobilephone="mobilephone"><br>
+Email Address:
+<br><input type="text" email="email"><br>
+<input type="submit" value="Submit">
+</form>
+</div>
+</div>
+<footer>
+<div id= contacts>
+<h3>Contact Info:</h3>
+<p>Phone: 07 4724 2086<br>
+Mobile: 0402 255 182<br><br>
+Postal Address PO Box:<br>
+1006, Townsville, Qld 4810<br>
+Address Townsville Civic Theatre:<br>
+41 Boundary Street, Townsville, Qld 4810<br>
+Office hours: 9:30 - 2:30 Monday - Wednesday</p>
+</div>
+<div id= sponsors>
+<h3>Sponsors:</h3>
+<h4>Townsville City Council:</h4>
+<p> http://www.townsville.qld.gov.au/Pages/default.aspx</p>
+<h4>Queensland Government Gambling Community Benefit Fund:</h4>
+<p>http://www.justice.qld.gov.au/corporate/sponsorships-and-grants/grants/community-benefit-funding-programs</p>
+</div>
+<div id="sitemap">
+<a href="index.php"><p id="activepage">Home</p></a>
+<a href="events.php"><p>Events</p></a>
+<a href="bulletin.php"><p>Bulletin Board</p></a>
+<a href="musicians.php"><p>Musicians</p></a>
+<a href="aboutUs.php"><p>About Us</p></a>
+<p>link 6</p>
+</div>
+</footer>
+</body>
+</html>
