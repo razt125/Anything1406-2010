9d3e5fb3e5f749bc0aafca6b55c050a862d4d903
jc239342 <leilei.deng@my.jcu.edu.au>
2015-05-27 12:35:56 +0800

    Create center.php

---
+++ center.php
@@ -0,0 +1,30 @@
+<?php
+require 'inc/init.class.php';
+if ( !checkLogin() )
+{
+    Lib_MsgError::setMsg('no login');
+}
+
+//check this user exists
+$userinfo = checkUserStatus($_SESSION['username']);
+
+if ( $userinfo['status'] == 1 )
+{
+    switch ($userinfo['level'])
+    {
+        //user
+        case 1:
+            //
+        case 2:
+            //admin
+        case 3:
+            $path = 'center.php';
+            break;
+        default:
+            Lib_MsgError::setMsg('not user level');
+            break;
+    }
+}
+include VIEWS_PATH . '/front/' . $path;
+
+require_once VIEWS_PATH . '/front/footer.php';
